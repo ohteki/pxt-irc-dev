282436134182e81178e92a51955889b05043ed9b
ohteki <ohteki@syun.jp>
2018-03-23 22:03:33 +0900

    test

---
+++ irc.ts
@@ -0,0 +1,9 @@
+//% weight=70 icon="\u30A2" color=#EC7505 block="IR"
+namespace IRC {
+
+    //% blockId=init block="test"
+    export function init(): void {
+		
+	}
+
+}
--- projects/main.ts
+++
@@ -1,9 +0,0 @@
-//% weight=70 icon="\u30A2" color=#EC7505 block="赤外線"
-namespace katakana {
-
-    //% blockId=init block="test"
-    export function init(): void {
-		
-	}
-
-}
